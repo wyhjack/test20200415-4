711940e5a6f142c7c81fdfd1e8a8d81a789f9264
wyhjack <909904682@qq.com>
2020-04-15 11:00:14 +0800

    IDEA中基于maven创建的java项目的初始化，将其提交到本地仓库中

---
+++ src/main/java/com/rj/bd/App.java
@@ -0,0 +1,20 @@
+package com.rj.bd;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import java.util.Map;
+public class App
+{
+    public static void main( String[] args ) throws JsonProcessingException {
+
+        System.out.println( "Hello World!" );
+        Map  map=new HashMap();
+        map.put("name", "杨过");
+        map.put("id", "001");
+
+        ObjectMapper mapper=new ObjectMapper();
+        String result = mapper.writeValueAsString(map);
+        System.out.println(result);
+    }
+}
